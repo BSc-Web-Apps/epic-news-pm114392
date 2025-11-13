e88e8832ce01ddfa2e8366c1507685c77342c14d
PM114392 <PM114392@hc.ac.je>
2025-11-13 12:00:50 +0000

    Header and Footer
    Button is submerged, need fix

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
+import FooterMenuRight from "./components/organisms/Footer/FooterMenuRight.tsx";
+import HeaderWithSearch from "./components/organisms/HeaderWithSearch/index.tsx";
 import Document from './components/shared-layout/Document.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
@@ -54,38 +56,40 @@ export default function App() {
 		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
+					<HeaderWithSearch/>
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">Your Journey Begins!</h1>
 
-            <div className="flex flex-row gap-8 pt-8 m-4">
-            <TeamMemberCard
-							name="Leonard Krasner"
-							role="Senior Designer"
-							imageSrc={headshot}
-						/>
-						<TeamMemberCard
-						name="John Doe"
-						role="Marketing Manager"
-						imageSrc={headshot2}
-						/>
-            <TeamMemberCard
-						name="Jenna Corey"
-						role="Finance Advisor"
-						imageSrc={headshot3}
-						/>
+            <div className="flex flex-col gap-8 pt-8 m-4 md:flex-row">
+							<TeamMemberCard
+								name="Leonard Krasner"
+								role="Senior Designer"
+								imageSrc={headshot}
+							/>
+							<TeamMemberCard
+								name="John Doe"
+								role="Marketing Manager"
+								imageSrc={headshot2}
+							/>
+							<TeamMemberCard
+								name="Jenna Corey"
+								role="Finance Advisor"
+								imageSrc={headshot3}
+							/>
 						</div>
 						<p className="lg:text-x1 text-base text-gray-600 md:text-lg">
 							Welcome to Epic News, where the latest developements in tech are
 							found.
 						</p>
-						<button className="rounded-full bg-amber-200 px-4 py-2 text-white md:px-6 md:py-3 lg:px-8 lg:py-4">
+						<button className="rounded-full bg-amber-200 px-4 py-2 text-white md:px-6 md:py-3 lg:px-8 lg:py-4 flex">
 							Learn More?
 						</button>
 					</main>
 				</div>
-				 <div className="container flex justify-between pb-5">
+				<div className="container flex justify-between pb-5">
           <ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
         </div>
+				<FooterMenuRight/>
 			</div>
 		</Document>
 	)
