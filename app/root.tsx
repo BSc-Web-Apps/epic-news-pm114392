92c60175719018179f934889c5ac3363ff4d36c1
PM114392 <PM114392@hc.ac.je>
2025-11-13 10:19:29 +0000

    Update root.tsx
    TeamMemberCard Component

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { RiTwitterXFill, RiLinkedinBoxFill } from "react-icons/ri";
 import { useLoaderData } from 'react-router'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
@@ -5,12 +6,47 @@ import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import Document from './components/shared-layout/Document.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
+import headshot from '~/assets/jpg/portrait-01.jpg'
+import headshot2 from '~/assets/jpg/portrait-02.jpg'
+import headshot3 from '~/assets/jpg/portrait-03.jpg'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
 }
 export { meta } from './__root.client.tsx'
 export { headers, loader } from './__root.server.tsx'
+
+interface TeamMemberCardProps {
+  name: string;
+  role: string;
+  imageSrc: string;
+}
+
+export function TeamMemberCard({
+  name,
+  role,
+  imageSrc,
+}: TeamMemberCardProps) {
+  return (
+    <div className="rounded-lg bg-slate-800 p-8 w-fit">
+      <img
+        src={imageSrc}
+        alt="An employee"
+        className="w-64 h-64 rounded-full mx-auto"
+      />
+
+      <div className="pt-6">
+        <h3 className="text-center font-semi-bold text-white">{name}</h3>
+        <p className="text-slate-400 text-center pt-1">{role}</p>
+
+        <div className="flex justify-center gap-4 text-slate-400 pt-6">
+          <RiTwitterXFill />
+          <RiLinkedinBoxFill />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function App() {
 	const data = useLoaderData<typeof loader | null>()
@@ -22,10 +58,29 @@ export default function App() {
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">Your Journey Begins!</h1>
+
+
 						<p className="lg:text-x1 text-base text-gray-600 md:text-lg">
 							Welcome to Epic News, where the latest developements in tech are
 							found.
 						</p>
+
+						<TeamMemberCard
+							name="Leonard Krasner"
+							role="Senior Designer"
+							imageSrc={headshot}
+						/>
+
+						<TeamMemberCard
+						name="John Doe"
+						role="Marketing Manager"
+						imageSrc={headshot2}/>
+
+            <TeamMemberCard
+						name="Jenna Corey"
+						role="Finance Advisor"
+						imageSrc={headshot3}/>
+
 						<button className="rounded-full bg-amber-200 px-4 py-2 text-white md:px-6 md:py-3 lg:px-8 lg:py-4">
 							Learn More?
 						</button>
