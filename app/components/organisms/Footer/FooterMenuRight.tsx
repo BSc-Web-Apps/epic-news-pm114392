a367c06912273d52068e0ddcc7cd630150c9bdb5
PM114392 <PM114392@hc.ac.je>
2025-11-20 10:24:55 +0000

    Logo added to Header and footer

--- app/components/organisms/Footer/FooterMenuRight.tsx
+++ app/components/organisms/Footer/FooterMenuRight.tsx
@@ -1,30 +1,30 @@
 import { Link, NavLink } from 'react-router'
-import logo from '#app/assets/svg/icon-placeholder.svg'
 import SocialMediaButtons from '#app/components/molecules/SocialMediaButtons'
 import { Button } from '#app/components/ui/button.tsx'
 import { type FooterProps } from './FooterBasic'
+import logo from '~/assets/png/placeholder-image.png'
 
 const FooterMenuRight = ({
 	companyName = 'Epic News!',
-	altText = 'Our company logo',
+	altText = 'Epic News Logo',
 }: FooterProps) => {
 	return (
 		<footer className="bg-secondary dark:bg-dark-secondary lg:py-16">
 			<div className="border-muted-foreground/75 dark:border-dark-muted-foreground/75 container items-center justify-between border-b py-8 lg:flex">
 				<Link to="/" className="flex w-20 items-center justify-center lg:w-24">
-					<img src={logo} alt={altText} />
+					<img src={logo} alt='Epic News Logo' className="w-16"/>
 				</Link>
 
 				<div className="lg:flex">
 					<div className="text-secondary-foreground dark:text-dark-secondary-foreground flex items-start gap-6 py-8 font-bold lg:mr-24">
 						<div>
-							<NavLink to="#">Nav Label</NavLink>
+							<NavLink to="#">About Us</NavLink>
 						</div>
 						<div>
-							<NavLink to="#">Nav Label</NavLink>
+							<NavLink to="#">News</NavLink>
 						</div>
 						<div>
-							<NavLink to="#">Nav Label</NavLink>
+							<NavLink to="#">Policies</NavLink>
 						</div>
 					</div>
 
--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -3,6 +3,7 @@ import { SearchBar } from '#app/components/search-bar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { UserDropdown } from '#app/components/user-dropdown.tsx'
 import { useOptionalUser } from '#app/utils/user.ts'
+import logo from '~/assets/png/placeholder-image.png';
 
 export default function HeaderWithSearch() {
 	const matches = useMatches()
@@ -13,7 +14,8 @@ export default function HeaderWithSearch() {
 	return (
 		<header className="container py-6">
 			<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-				<div>
+				<div className="flex items-center justify-center gap-4">
+					<img src={logo} alt="Epic News Logo" className="w-16"/>
         <span className="text-sm text-foreground">Epic News</span>
         </div>
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
