3e841f026888c9fc426ac64a574a93e51ecc95a6
PM114392 <PM114392@hc.ac.je>
2025-11-20 12:15:35 +0000

    Implemented heroImage to Root

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,8 @@ import rootLinkElements from './utils/providers/rootLinkElements.ts'
 import headshot from '~/assets/jpg/portrait-01.jpg'
 import headshot2 from '~/assets/jpg/portrait-02.jpg'
 import headshot3 from '~/assets/jpg/portrait-03.jpg'
-
+import heroImage from '~/assets/jpg/sample-hero.jpg'
+import HeroCallToAction from "~/components/organisms/Hero/HeroCallToAction.tsx";
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
 }
@@ -59,6 +60,18 @@ export default function App() {
 					<HeaderWithSearch/>
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">Your Journey Begins!</h1>
+
+            <div className="w-full py-16">
+							<HeroCallToAction image={heroImage} 
+							imageRight={true}>
+                <div className="flex flex-col gap-8 px-8">
+                  <h2 className="text-h2">Welcome to Epic News</h2>
+                  <p className="text-lg">
+                    Keep up to date with the latest tech news.
+                  </p>
+                </div>
+              </HeroCallToAction>
+            </div>
 
             <div className="flex flex-col gap-8 pt-8 m-4 md:flex-row">
 							<TeamMemberCard
