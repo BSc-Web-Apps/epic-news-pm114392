849b46f98a709392090188cf6a7a415cb8686b32
PM114392 <PM114392@hc.ac.je>
2025-11-27 12:14:54 +0000

    Made a wireframe layout on Technology in the news web page

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -1,7 +1,36 @@
+import { invariant } from '@epic-web/invariant'
+import { type LoaderFunctionArgs, data, useLoaderData } from 'react-router'
+import { toTitleCase } from '~/utils/stringUtils.ts'
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const { category } = params
+
+  invariant(typeof category === 'string', 'Category not found')
+    const categoryTitle = toTitleCase(category)
+
+   return data({ categoryTitle })
+}
+
+const WireframeBlock = () => {
+  return (
+    <div className="h-72 animate-pulse rounded-lg bg-gray-200 dark:bg-gray-700" />
+    
+  )
+  
+}
 export default function NewsCategoryPage() {
+  const { categoryTitle } = useLoaderData<typeof loader>()
+
   return (
     <div className="container py-16">
-      <h2 className="text-h2">Generic news category page</h2>
+      <h2 className="text-h2">{categoryTitle}</h2>
+      <div className="grid grid-cols-5 gap-6">
+  <WireframeBlock />
+  <WireframeBlock />
+  <WireframeBlock />
+  <WireframeBlock />
+  <WireframeBlock />
+</div>
     </div>
   )
 }
