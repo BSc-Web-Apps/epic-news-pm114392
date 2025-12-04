498fe703900f2cc2eec5827c75c129acff7f2c5c
PM114392 <PM114392@hc.ac.je>
2025-12-04 12:13:29 +0000

    Add category selection
    Red or yellow lines in the following areas: Schema.prisma, root.tsx, articles.new.tsx, news.$category.tsx, login.tsx.

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -1,6 +1,7 @@
 import { invariant } from '@epic-web/invariant'
 import { type LoaderFunctionArgs, data, useLoaderData } from 'react-router'
 import { toTitleCase } from '~/utils/stringUtils.ts'
+import { prisma } from '~/utils/db.server.ts'
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { category } = params
@@ -8,28 +9,43 @@ export async function loader({ params }: LoaderFunctionArgs) {
   invariant(typeof category === 'string', 'Category not found')
     const categoryTitle = toTitleCase(category)
 
-   return data({ categoryTitle })
+      const allArticles = await prisma.article.findMany({
+    select: {
+      id: true,
+      title: true,
+      category: { select: { name: true } },
+      images: { select: { id: true } },
+    },
+  })
+
+   return data({ categoryTitle, allArticles })
 }
 
-const WireframeBlock = () => {
-  return (
-    <div className="h-72 animate-pulse rounded-lg bg-gray-200 dark:bg-gray-700" />
+// const WireframeBlock = () => {
+//   return (
+//     <div className="h-72 animate-pulse rounded-lg bg-gray-200 dark:bg-gray-700" />
     
-  )
+//   )
   
-}
+// }
 export default function NewsCategoryPage() {
-  const { categoryTitle } = useLoaderData<typeof loader>()
+  const { categoryTitle, allArticles } = useLoaderData<typeof loader>()
 
   return (
     <div className="container py-16">
       <h2 className="text-h2">{categoryTitle}</h2>
       <div className="grid grid-cols-5 gap-6">
+        {allArticles.map(article => (
+          <div key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.category?.name || 'General News'}</p>
+          </div>
+        ))}
+  {/* <WireframeBlock />
   <WireframeBlock />
   <WireframeBlock />
   <WireframeBlock />
-  <WireframeBlock />
-  <WireframeBlock />
+  <WireframeBlock /> */}
 </div>
     </div>
   )
